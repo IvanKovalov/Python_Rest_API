3e3028c1d7aae68ca37650a5b90de8f4120d3c7a
IvanKovalov <kovalevvana8@gmail.com>
2022-11-20 21:32:11 +0300

    + added validation

--- application/dto/RecordDto.py
+++ application/dto/RecordDto.py
@@ -1,30 +1,31 @@
-class RecordDTO:
-    def __init__(self):
-        self.user_id = None
-        self.category_id = None
-        self.record_create_date = None
-        self.record_sum = None
+import logging
+import numbers
+
+from marshmallow import Schema, fields, validate
+
+from application.validator.RecordValidator import UsersIDValidator, CategoriesIDValidator
+
+
+class RecordDTO(Schema):
+    id = fields.Str(dump_only=True)
+    userId = UsersIDValidator()
+    recordSum = fields.Integer(required=True)
+    categoryId = CategoriesIDValidator()
 
     def set_user(self, user_id):
-        self.user_id = user_id
+        self.userId = user_id
 
     def set_category(self, category_id):
-        self.category_id = category_id
-
-    def set_create_date(self, create_date):
-        self.record_create_date = create_date
+        self.categoryId = category_id
 
     def set_sum(self, record_sum):
-        self.record_sum = record_sum
+        self.recordSum = record_sum
 
     def get_user_id(self):
-        return self.user_id
+        return self.userId
 
     def get_category_id(self):
-        return self.category_id
-
-    def get_create_date(self):
-        return self.record_create_date
+        return self.categoryId
 
     def get_sum(self):
-        return self.record_sum
+        return self.recordSum
