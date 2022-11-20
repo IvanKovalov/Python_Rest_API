cba6578c80c05b0cab5d4e1d9b41694b6b352e67
IvanKovalov <kovalevvana8@gmail.com>
2022-11-20 21:33:24 +0300

    * changed services and repository logic to db

--- application/service/CategoryService.py
+++ application/service/CategoryService.py
@@ -1,38 +1,36 @@
 import logging
 
 from application.entity.CategoryEntity import CategoryEntity
+from application.models.CategoryModel import CategoryModel
+from application.models.database import db
 from application.repository.CategoryRepository import CategoryRepository
 
 
 class CategoryService:
     def __init__(self):
-        self.category_repository = CategoryRepository()
+        self.category_repository = CategoryRepository(db)
         self.category_counter = 1
         self.log = logging.getLogger(CategoryService.__name__)
         self.log.info("Created new instance of Category Service")
 
     def save_category(self, category_dto):
-        category_entity = CategoryEntity()
-        category_entity.set_id(self.category_counter)
-        category_entity.set_name(category_dto.__get_name__())
-        if category_dto.__get_name__() is None:
-            self.log.error("Wrong data")
-            return 404
-        self.log.info("Saving Category id dict")
-        self.category_repository.save_category(category_entity)
-        self.category_counter = self.category_counter + 1
+        category = CategoryModel(**category_dto)
+        self.category_repository.save_category(category)
+        return category
 
     def get_category(self, category_id):
-        if type(category_id) is int:
-            if category_id in self.category_repository.category_map.keys():
-                self.log.info("Find category by id" + category_id)
-                return self.category_repository.get_category_by_id(int(category_id))
-            else:
-                self.log.error("No category with such id")
-                return Exception('No category with such id')
-        else:
-            self.log.error("Id not integer")
-            return Exception('Id not integer')
+        # if type(category_id) is int:
+        #     if category_id in self.category_repository.category_map.keys():
+        #         self.log.info("Find category by id" + category_id)
+        #         return self.category_repository.get_category_by_id(int(category_id))
+        #     else:
+        #         self.log.error("No category with such id")
+        #         return Exception('No category with such id')
+        # else:
+        #     self.log.error("Id not integer")
+        #     return Exception('Id not integer')
+        self.log.info("Get category by id: " + category_id)
+        return self.category_repository.get_category_by_id(int(category_id))
 
     def get_all(self):
         self.log.info("Get all category")
